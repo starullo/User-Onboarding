7bef53b731f40ea79acdb6170d6c54e9d523aabc
Sam Tarullo <sam@Sams-Air.attlocal.net>
2020-09-17 15:33:57 -0500

    finished with main tasks

---
+++ user-onboarding/cypress/integration/examples/form_test.js
@@ -0,0 +1,43 @@
+describe('User Onboarding Project Page', () => {
+    beforeEach(()=>{
+        cy.visit('http://localhost:3000')
+    })
+    
+    const nameInput = () => cy.get('input[type=text]');
+    const emailInput = () => cy.get('input[type=email]')
+    const passwordInput = () => cy.get('input[type=password]')
+    const checkboxInput = () => cy.get('input[type=checkbox]');
+    const submit = () => 
+        cy.get('button')
+    
+
+    it('testing', () => {
+        nameInput().should('exist')
+        emailInput().should('exist')
+        passwordInput().should('exist')
+        checkboxInput().should('exist')
+    })
+
+    it('testing inputs', () => {
+        nameInput().type('Sam Tarullo').should('have.value', 'Sam Tarullo');
+
+        emailInput().type('starullo87@gmail.com').should('have.value', 'starullo87@gmail.com');
+
+        passwordInput().type('password').should('have.value', 'password')
+
+        checkboxInput().check();
+
+        submit().click();
+
+        nameInput().should('not.have.value', '')
+
+        emailInput().should('not.have.value', '')
+
+        passwordInput().should('not.have.value', '')
+
+        checkboxInput().should('not.have.value', 'false')
+
+    })
+
+})
+
